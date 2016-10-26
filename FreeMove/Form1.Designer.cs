611cf968ff37cb540e01f1fd6d921be67b0f4ba9
imDema <luca.de.martini@protonmail.com>
2016-10-26 16:22:41 +0200

    Initial Commit

--- FreeMove/Form1.Designer.cs
+++ FreeMove/Form1.Designer.cs
@@ -28,12 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_From = new System.Windows.Forms.TextBox();
+            this.button_BrowseFrom = new System.Windows.Forms.Button();
+            this.button_BrowseTo = new System.Windows.Forms.Button();
+            this.textBox_To = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button_Move = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Move From:";
+            // 
+            // textBox_From
+            // 
+            this.textBox_From.Location = new System.Drawing.Point(83, 13);
+            this.textBox_From.Name = "textBox_From";
+            this.textBox_From.Size = new System.Drawing.Size(383, 20);
+            this.textBox_From.TabIndex = 1;
+            // 
+            // button_BrowseFrom
+            // 
+            this.button_BrowseFrom.Location = new System.Drawing.Point(472, 13);
+            this.button_BrowseFrom.Name = "button_BrowseFrom";
+            this.button_BrowseFrom.Size = new System.Drawing.Size(75, 23);
+            this.button_BrowseFrom.TabIndex = 2;
+            this.button_BrowseFrom.Text = "Browse...";
+            this.button_BrowseFrom.UseVisualStyleBackColor = true;
+            this.button_BrowseFrom.Click += new System.EventHandler(this.button_BrowseFrom_Click);
+            // 
+            // button_BrowseTo
+            // 
+            this.button_BrowseTo.Location = new System.Drawing.Point(472, 39);
+            this.button_BrowseTo.Name = "button_BrowseTo";
+            this.button_BrowseTo.Size = new System.Drawing.Size(75, 23);
+            this.button_BrowseTo.TabIndex = 4;
+            this.button_BrowseTo.Text = "Browse...";
+            this.button_BrowseTo.UseVisualStyleBackColor = true;
+            this.button_BrowseTo.Click += new System.EventHandler(this.button_BrowseTo_Click);
+            // 
+            // textBox_To
+            // 
+            this.textBox_To.Location = new System.Drawing.Point(83, 39);
+            this.textBox_To.Name = "textBox_To";
+            this.textBox_To.Size = new System.Drawing.Size(383, 20);
+            this.textBox_To.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "To:";
+            // 
+            // button_Move
+            // 
+            this.button_Move.Location = new System.Drawing.Point(472, 95);
+            this.button_Move.Name = "button_Move";
+            this.button_Move.Size = new System.Drawing.Size(75, 23);
+            this.button_Move.TabIndex = 6;
+            this.button_Move.Text = "Move";
+            this.button_Move.UseVisualStyleBackColor = true;
+            this.button_Move.Click += new System.EventHandler(this.button_Move_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(16, 65);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(154, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Set original folder to hidden";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(564, 130);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button_Move);
+            this.Controls.Add(this.button_BrowseTo);
+            this.Controls.Add(this.textBox_To);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button_BrowseFrom);
+            this.Controls.Add(this.textBox_From);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Free Move";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_From;
+        private System.Windows.Forms.Button button_BrowseFrom;
+        private System.Windows.Forms.Button button_BrowseTo;
+        private System.Windows.Forms.TextBox textBox_To;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button_Move;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- FreeMove/Form1.cs
+++ FreeMove/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace FreeMove
 {
@@ -15,6 +18,114 @@ namespace FreeMove
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button_Move_Click(object sender, EventArgs e)
+        {
+            string from, to;
+            from = textBox_From.Text;
+            to = textBox_To.Text;
+
+            if (CheckFolders(from, to ))
+            {
+                Directory.Move(from, to);
+                Process mkink = new Process();
+                mkink.StartInfo.FileName = "cmd.exe";
+                mkink.StartInfo.Arguments = "/c mklink /j " + from + " " + to;
+                mkink.StartInfo.UseShellExecute = false;
+                mkink.StartInfo.RedirectStandardOutput = true;
+                mkink.Start();
+
+                string output = mkink.StandardOutput.ReadToEnd();
+                mkink.WaitForExit();
+                WriteLog(output);
+                MessageBox.Show(ReadLog());
+            }
+            else
+            {
+                textBox_From.Text = "";
+                textBox_To.Text = "";
+                textBox_From.Focus();
+            }
+        }
+
+        private bool CheckFolders(string frompath, string topath)
+        {
+            bool passing = true;
+            string errors = "";
+            try
+            {
+                Path.GetFullPath(frompath);
+                Path.GetFullPath(topath);
+            }
+            catch (Exception)
+            {
+                errors += "ERROR, invalid path name\n";
+                passing = false;
+            }
+            string pattern = "^[A-Z]:\\\\";
+            if (!Regex.IsMatch(frompath,pattern) || !Regex.IsMatch(topath,pattern))
+            {
+                errors += "ERROR, invalid path format";
+                passing = false;
+            }
+
+            if (!Directory.Exists(frompath))
+            {
+                errors += "ERROR, source folder doesn't exist";
+                passing = false;
+            }
+            if (Directory.Exists(topath))
+            {
+                errors += "ERROR, destination folder already exists";
+                passing = false;
+            }
+            if(!Directory.Exists(Directory.GetParent(topath).FullName))
+            {
+                errors += "ERROR, parent of destination folder doesn't exist";
+                passing = false;
+            }
+
+            if (!passing)
+                MessageBox.Show(errors);
+
+            return passing;
+        }
+
+        private void button_BrowseFrom_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox_From.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button_BrowseTo_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox_To.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void WriteLog(string log)
+        {
+            File.WriteAllText(GetTempFolder() + @"\log.log", log);
+        }
+
+        private string ReadLog()
+        {
+            return File.ReadAllText(GetTempFolder() + @"\log.log");
+        }
+
+        private string GetTempFolder()
+        {
+            string dir = Environment.GetEnvironmentVariable("TEMP") + @"\FreeMove";
+            //if(!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+            return dir;
         }
     }
 }
